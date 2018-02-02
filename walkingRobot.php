cbc57546568f0e6ec7589b50e287c7304a096e7b
amrapali.nangare <amrapali.nangare@assetvantage.com>
2018-02-03 01:14:33 +0530

    added README file

--- walkingRobot.php
+++ walkingRobot.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function main($argv) {
 
     define('NORTH', 0);
@@ -9,41 +8,46 @@ function main($argv) {
     define('WEST', 3);
 
     unset($argv[0]);
-    $x = $argv[1];
-    $y = $argv[2];
-    $dir = constant($argv[3]);
-    $path = str_split($argv[4], 1); //RW2LW4RR
-    foreach ($path as $key => $move) {
-        if ($move == 'R') {
-            $dir = ($dir + 1) % 4;
-            $direction = getDirection($dir);
-        } elseif ($move == 'L') {
-            $dir = (4 + $dir - 1) % 4;
-             $direction = getDirection($dir);
-        } else {
-            $next = $key + 1;
-            if ($dir == 0) {
-                $y = $y + $path[$next];
-            } else if ($dir == 1) {
-                $x = $x + $path[$next];
-            } else if ($dir == 2) {
-                $y = $y - $path[$next];
-            } else{ // dir == W
-                $x = $x - $path[$next];
+    // check for proper input
+    if (empty($argv) || empty($argv[1]) || empty($argv[2]) || empty($argv[3]) || empty($argv[4])) {
+        echo "Please provide proper input";
+    } else {
+        $x = $argv[1];
+        $y = $argv[2];
+        $dir = constant(strtoupper($argv[3]));
+        $path = str_split($argv[4], 1); 
+        foreach ($path as $key => $move) {
+            if ($move == 'R') {
+                $dir = ($dir + 1) % 4;
+                $direction = getDirection($dir);
+            } elseif ($move == 'L') {
+                $dir = (4 + $dir - 1) % 4;
+                $direction = getDirection($dir);
+            } else {
+                $next = $key + 1;
+                if ($dir == 0) {
+                    $y = $y + $path[$next];
+                } else if ($dir == 1) {
+                    $x = $x + $path[$next];
+                } else if ($dir == 2) {
+                    $y = $y - $path[$next];
+                } else { // dir == W
+                    $x = $x - $path[$next];
+                }
+                $direction = getDirection($dir);
             }
-            $direction = getDirection($dir);
         }
+
+        print_r("X - axis : " . $x);
+        echo PHP_EOL;
+        print_r("Y - axis : " . $y);
+        echo PHP_EOL;
+        print_r($direction . " direction");
     }
-    
-    print_r("X - axis : " .$x);
-    echo PHP_EOL;
-    print_r("Y - axis : ".$y);
-    echo PHP_EOL;
-    print_r($direction. " direction");
 }
 
-function getDirection($dir){
-    switch ($dir){
+function getDirection($dir) {
+    switch ($dir) {
         case 0 :
             $direction = 'NORTH';
             break;
@@ -59,6 +63,7 @@ function getDirection($dir){
     }
     return $direction;
 }
+
 main($argv);
 ?>
    
